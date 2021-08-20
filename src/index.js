cacf97fd9d453abdfe55ec7914637c2d4d2eacfb
YaryhaAD <YaryhaAD@gmail.com>
2021-08-20 17:29:02 +0300

    feat: create morse decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,14 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    return expr.match(/.{10}/g).map( (i) => {
+        if (i.charAt(0) === '*') {
+            return ' ';
+        }else return MORSE_TABLE[i.replace(/00/g,'').replace(/11/g,'-').replace(/10/g,'.')];
+    }).join('');
 }
 
 module.exports = {
     decode
 }
+
